b51683fa7281134108f2dd713e9b995df0956da6
Dev Borland <devborland@gmail.com>
2021-02-12 13:26:59 +0300

    Using LayoutBuilder

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -12,54 +12,61 @@ class ChartBar extends StatelessWidget {
   );
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 12.0,
-          child: FittedBox(
-            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+    return LayoutBuilder(builder: (ctx, constrains) {
+      return Column(
+        children: [
+          Container(
+            height: constrains.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+            ),
           ),
-        ),
-        SizedBox(height: 4.0),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            alignment: AlignmentDirectional.bottomCenter,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 0.0,
-                  ),
-                  color: Colors.grey.shade300,
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),
-              FractionallySizedBox(
-                alignment: Alignment.bottomCenter,
-                heightFactor: spendingPctOfTotal < 0 ? 0 : spendingPctOfTotal,
-                child: Container(
+          SizedBox(height: constrains.maxHeight * 0.05),
+          Container(
+            height: constrains.maxHeight * 0.6,
+            width: 10,
+            child: Stack(
+              alignment: AlignmentDirectional.bottomCenter,
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(8.0),
                     border: Border.all(
                       color: Colors.grey,
                       width: 0.0,
                     ),
+                    color: Colors.grey.shade300,
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
                 ),
-              ),
-            ],
+                FractionallySizedBox(
+                  alignment: Alignment.bottomCenter,
+                  heightFactor: spendingPctOfTotal < 0 ? 0 : spendingPctOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 0.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        SizedBox(height: 4.0),
-        Text(
-          label,
-          style: TextStyle(color: Colors.grey.shade500),
-        ),
-      ],
-    );
+          SizedBox(height: constrains.maxHeight * 0.05),
+          Container(
+            height: constrains.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text(
+                label,
+                style: TextStyle(color: Colors.grey.shade500),
+              ),
+            ),
+          ),
+        ],
+      );
+    });
   }
 }
